cae75e04f230519b4abd58ff3d6b299c2a782ecb
Ning <jianing772@gmail.com>
2022-01-06 14:52:49 -0600

    Update API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  componentDidMount(){
-    console.log(123);
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -13,7 +23,7 @@ class App extends Component {
       <div className="App">
         <Navbar /> 
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
