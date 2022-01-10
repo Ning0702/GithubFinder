7ffa33618eccecd5a795c608614a268b356dad74
Ning <jianing772@gmail.com>
2022-01-10 14:37:02 -0600

    Use Context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+    const githubContext = useContext(GithubContext);
+
+    const { getUser, loading, user, repos, getUserRepos } = githubContext;
+
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);
@@ -38,7 +42,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
             {hireable ? <i className="fas fa-check text-success" /> : <i className="fas fa-times-circle text-danger" />}
             <div className='card grid-2'>
                 <div className="all-center">
-                    <img src={avatar_url} className='round-imag' alt="" style={{ width: '150px'}} />
+                    <img src={avatar_url} className='round-imag' alt="" style={{ width: '150px' }} />
                     <h1>{name}</h1>
                     <p>Location: {location}</p>
                 </div>
@@ -70,7 +74,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
                 </div>
             </div>
             <div className="card text-center">
-                <div className="badge badge-primary">Followers: {followers}</div> 
+                <div className="badge badge-primary">Followers: {followers}</div>
                 <div className="badge badge-success">Following: {following}</div>
                 <div className="badge badge-light">Public Repos: {public_repos}</div>
                 <div className="badge badge-dark">Public Gists: {public_gists}</div>
@@ -78,14 +82,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
             <Repos repos={repos} />
         </Fragment>
     );
-}
-
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
 };
 
-export default User
+export default User;
